16daac6f9f5212297dda698c29dc1daa6bba10b2
DemonGamingLP <demongaminglp@gmail.com>
2024-09-06 22:08:34 +0200

    remove app router

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,6 @@ import createEmotionCache from '../utility/createEmotionCache';
 import darkTheme from '../styles/theme/darkTheme';
 import '../styles/globals.css';
 import AppProvider from '@components/common/context/AppProvider';
-import WaitForRouter from '../components/common/WaitForRouter';
 import NavBar from '../components/common/NavBar';
 
 const clientSideEmotionCache = createEmotionCache();
@@ -20,14 +19,13 @@ const MyApp = (props) => {
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={darkTheme}>
           <EmotionThemeProvider theme={darkTheme}>
-            <CssBaseline/>
-            <WaitForRouter>
+            <CssBaseline>
               <AppProvider>
                 <NavBar>
                   <Component {...pageProps} />
                 </NavBar>
               </AppProvider>
-            </WaitForRouter>
+            </CssBaseline>
           </EmotionThemeProvider>
         </ThemeProvider>
       </CacheProvider>
